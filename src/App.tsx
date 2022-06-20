209e4651550cba5c63a31351205aa8d0022b1083
Xyzjesus <62913761+Xyzjesus@users.noreply.github.com>
2022-06-20 11:10:17 +0300

    Update App.tsx
    Signed-off-by: Xyzjesus <62913761+Xyzjesus@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDY-qQ3cSPkesRnLDzhk87WZvfgoz2udgM",
-  authDomain: "code-punks-online-school.firebaseapp.com",
-  projectId: "code-punks-online-school",
-  storageBucket: "code-punks-online-school.appspot.com",
-  messagingSenderId: "705581633580",
-  appId: "1:705581633580:web:c255fae48f100643b38189",
-  measurementId: "G-3J5J90M16D"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
